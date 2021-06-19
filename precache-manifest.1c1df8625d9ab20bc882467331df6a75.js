271b1660befaa98ac8240515581314962ee7a62d
Renato Soares <renatosoarespro@hotmail.com>
2021-06-19 07:14:32 -0300

    deploy

---
+++ precache-manifest.1c1df8625d9ab20bc882467331df6a75.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511dce29df9a695a479b",
+    "url": "/css/about.4505df98.css"
+  },
+  {
+    "revision": "339f76994f35717c9f4e",
+    "url": "/css/app.4ea91f2f.css"
+  },
+  {
+    "revision": "4904b24b7daad0c723f7",
+    "url": "/css/chunk-vendors.832237ea.css"
+  },
+  {
+    "revision": "b124e4a18dddc4e8ee9af3d4f6162a95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "511dce29df9a695a479b",
+    "url": "/js/about.826229a0.js"
+  },
+  {
+    "revision": "339f76994f35717c9f4e",
+    "url": "/js/app.f9e466e6.js"
+  },
+  {
+    "revision": "4904b24b7daad0c723f7",
+    "url": "/js/chunk-vendors.c72e7ea1.js"
+  },
+  {
+    "revision": "5e6cc35d1526ebcf193eb324eb6ca2a5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
