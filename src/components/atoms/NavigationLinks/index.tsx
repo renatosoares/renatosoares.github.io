487f6f3be49fa4aabd5a221681f3759b896a8e96
Renato Soares <renatosoarespro@hotmail.com>
2021-08-06 21:08:56 -0300

    add component navigation links

---
+++ src/components/atoms/NavigationLinks/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link, useRouteMatch } from "react-router-dom";
+
+import NavigationLinksStyles from "./NavigationLinks.module.scss";
+import "./styles.scss";
+
+const NavigationLinks = () => {
+  return (
+    <div
+      className={[
+        NavigationLinksStyles.navigationLinks,
+        "navigation-links",
+      ].join(" ")}
+    >
+      <Link className={useRouteMatch("/")?.isExact ? "active" : ""} to="/">
+        Home
+      </Link>
+      <Link
+        className={useRouteMatch("/about")?.isExact ? "active" : ""}
+        to="/about"
+      >
+        About
+      </Link>
+    </div>
+  );
+};
+
+export default NavigationLinks;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,21 +4,26 @@ import { Switch, Route } from "react-router-dom";
 import Home from "routes/home";
 import Error404 from "routes/error404";
 
+import NavigationLinks from "components/atoms/NavigationLinks";
+
 const AppRoutes = () => (
-  <Switch>
-    <Route exact path="/">
-      <Home />
-    </Route>
-    <Route path="/about">
-      <div>About</div>
-    </Route>
-    <Route path="/production">
-      <div>Production</div>
-    </Route>
-    <Route path="*">
-      <Error404 />
-    </Route>
-  </Switch>
+  <>
+    <NavigationLinks></NavigationLinks>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <div>About</div>
+      </Route>
+      <Route path="/production">
+        <div>Production</div>
+      </Route>
+      <Route path="*">
+        <Error404 />
+      </Route>
+    </Switch>
+  </>
 );
 
 export default AppRoutes;
