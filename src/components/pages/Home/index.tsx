abdc42c1dbee95fa34777c80af10b434cd7b2fa7
Renato Soares <renatosoarespro@hotmail.com>
2021-08-13 09:26:55 -0300

    add component coding activity

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import SocialMy from "components/atoms/SocialMy";
 import CalendarGithub from "components/atoms/CalendarGithub";
+import CodingActivity from "components/atoms/Charts/CodingActivity";
 
 const Home = () => {
   return (
     <>
       <CalendarGithub />
+      <CodingActivity />
       <SocialMy />
     </>
   );
